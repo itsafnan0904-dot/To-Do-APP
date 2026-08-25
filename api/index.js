e90454a449c92fb4f1e8ae04544483e681e6c605
itsafnan0904-dot <itsafnan0904@gmail.com>
2026-08-25 14:41:28 +0500

    Fix Linux case-sensitive module imports

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/*splat", cors(corsOptions));
 
 app.use(express.json());
 
--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/*splat", cors(corsOptions));
 
 app.use(express.json());
 
